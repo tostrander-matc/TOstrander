31f8d9736c6c997fe3cf0caef98baf1301908c85
Tim <tostrander@madisoncollege.edu>
2021-02-05 14:49:10 -0600

    Week 2 Assignments

---
+++ scriptname1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+prints "Hello World" to the screen
+Usage: ./scriptname1.py
+"""
+
+print("Hello World")
---
+++ week2-script.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""
+prints "Tim O" to the screen
+Usage: ./week2-script.py
+"""
+print("Tim Ostrander. I learned that Python isn't easy.")
